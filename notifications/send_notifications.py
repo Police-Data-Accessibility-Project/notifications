67b337043f93aedc3d94a3717bc346115c15de32
maxachis <maxachis@gmail.com>
2024-11-20 07:50:59 -0500

    Create first automation job -- notifications

---
+++ notifications/send_notifications.py
@@ -0,0 +1,31 @@
+import requests
+import argparse
+
+BASE_ROUTE = 'https://data-sources-v2.pdap.io/api'
+
+def login(email: str, password: str) -> str:
+    login_route = f'{BASE_ROUTE}/login'
+    payload = {'email': email, 'password': password}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(login_route, json=payload, headers=headers)
+    response.raise_for_status()
+    return response.json()['access_token']
+
+def call_notifications_endpoint(jwt_token: str):
+    notifications_route = f'{BASE_ROUTE}/notifications'
+    headers = {'Authorization': f'Bearer {jwt_token}'}
+    response = requests.post(notifications_route, headers=headers)
+    response.raise_for_status()
+    print(response.json())
+
+def send_notifications(email: str, password: str):
+    jwt_token = login(email, password)
+    call_notifications_endpoint(jwt_token)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--email', type=str, required=True)
+    parser.add_argument('--password', type=str, required=True)
+    args = parser.parse_args()
+    send_notifications(args.email, args.password)
