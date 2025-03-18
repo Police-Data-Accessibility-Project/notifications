a8aaf5aad2c984a5216eb4afb53119a83e64ed5c
Max Chis <maxachis@gmail.com>
2025-03-18 19:26:07 -0400

    Reconfigure dockerfile

--- notifications/runner.sh
+++ notifications/runner.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-python3 notifications/send_notifications.py --email $NOTIFICATIONS_APP_USER_EMAIL --password $NOTIFICATIONS_APP_USER_PASSWORD
+python3.11 -m  notifications.send_notifications.py --email $NOTIFICATIONS_APP_USER_EMAIL --password $NOTIFICATIONS_APP_USER_PASSWORD
