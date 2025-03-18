147fd162380cd5adb06d93168b2d833008023def
Max Chis <maxachis@gmail.com>
2025-03-18 19:03:28 -0400

    Reconfigure dockerfile

--- github_data_requests_synchronize/runner.sh
+++ github_data_requests_synchronize/runner.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-python3.11 -m github_data_requests_synchronize/synchronize.py --email $GITHUB_SYNCHRONIZE_APP_USER_PASSWORD --password $GITHUB_SYNCHRONIZE_APP_USER_PASSWORD
+python3.11 -m github_data_requests_synchronize/synchronize --email $GITHUB_SYNCHRONIZE_APP_USER_PASSWORD --password $GITHUB_SYNCHRONIZE_APP_USER_PASSWORD
